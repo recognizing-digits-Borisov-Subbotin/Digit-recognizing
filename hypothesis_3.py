e13615560e0a06df334d864e06fa14a3983ee88c
fiddler811 <andrey.05.12.93@mail.ru>
2016-12-12 15:31:07 +0500

    Update hypothesis_3.py
    Сделаны изменения, аналогичные с файлом hypothesis_2.py (разбиение тестовой картинки на отдельные элементы и проверка их на принадлежность к исходному шрифту, вывод результатов).

--- hypothesis_3.py
+++ hypothesis_3.py
@@ -6,12 +6,18 @@ from __future__ import print_function
 import cv2
 import numpy as np
 
-# relative module
-import video
-
 # built-in module
 import sys
 
+def split2d(img, cell_size, flatten=True):
+    h, w = img.shape[:2]
+    sx, sy = cell_size
+    cells = [np.hsplit(row, w//sx) for row in np.vsplit(img, h//sy)]
+    cells = np.array(cells)
+    if flatten:
+        cells = cells.reshape(-1, sy, sx)
+    #print (cells)
+    return cells
 
 if __name__ == '__main__':
     print(__doc__)
@@ -77,3 +83,39 @@ if __name__ == '__main__':
     a = sorted(a, key = cv2.contourArea, reverse = True)[:1]
     area_5 = cv2.arcLength(a[0],True)
     print (area_5)
+
+    gray = cv2.cvtColor(img_test, cv2.COLOR_BGR2GRAY)
+    #edge = cv2.Canny(gray, thrs1, thrs2, apertureSize=5)
+    digits = split2d(gray, (50, 50))
+    print (len(digits))
+    h, w = gray.shape[:2]
+    print (w)
+    sx = 50
+    x = w/sx
+    mas_false = []
+    for c in digits:
+        edge = cv2.Canny(c, thrs1, thrs2, apertureSize=5)
+        (cnts,a,b) = cv2.findContours(edge.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        a = sorted(a, key = cv2.contourArea, reverse = True)[:1]
+        area_test = cv2.arcLength(a[0],True)
+        print (area_test)
+        if (abs(area_test - area_1) <= 5 or abs(area_test - area_2) <= 5 or abs(area_test - area_3) <= 5 or abs(area_test - area_4) <= 5 or abs(area_test - area_5) <= 5):
+            print ("true")
+            mas_false.append(1)
+        else:
+            print ("false")
+            mas_false.append(0)
+    count = 0
+    for i in mas_false:
+        if count < x:
+            print (i, end = ' ')
+            #print (" ")
+            count = count + 1
+        else:
+            count = 1
+            print ('\n', end = '')
+            print (i, end = ' ')
+    print ('\n', end = '')
+    cv2.imshow('edge', img_test)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
